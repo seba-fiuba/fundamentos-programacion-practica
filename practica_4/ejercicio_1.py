7d2da1219437cae51e5b5bb2a184243098b043e1
sebavc18 <villalbasebastianc@gmail.com>
2025-08-23 23:51:13 -0300

    actualizacion

---
+++ practica_4/ejercicio_1.py
@@ -0,0 +1,15 @@
+# A
+def par_o_impar(n):
+    "indica si el numero es par o impar"
+    if n % 2 == 0:
+        print(f"{n} es par")
+    else:
+        print(f"{n} es impar")
+
+
+par_o_impar(5)
+
+
+# B
+def indicar_si_es_primo(n):
+    "Indica si el numero es primo"
